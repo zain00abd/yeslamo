3417acfc4d009cf3bf7d28470249599d90af3343
zain00abd <zainabflash1998@gmail.com>
2026-04-02 17:38:06 +0300

    addr2

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -340,7 +340,7 @@ export default function HomePage() {
 
     function getGreeting() {
         const hour = new Date().getHours();
-        if (hour < 12) return "صباح الخير";
+        if (hour < 12) return "صاح الخير";
         if (hour < 18) return "مساء الخير";
         return "مساء الخير";
     }
